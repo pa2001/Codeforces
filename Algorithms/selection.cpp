aec727cefce562471831dfe95c64840d8a440848
Pragya Anand <86311707+pa2001@users.noreply.github.com>
2021-08-21 14:06:27 +0530

    Create selection.cpp

---
+++ Algorithms/selection.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  int a;
+  vector<int>v;
+  for(int i=0;i<n;i++)
+  {
+    cin>>a;
+    v.push_back(a);
+  }
+  int index=-1;
+  for(int i=0;i<n;i++)
+  {
+    index=i;
+    for(int j=i+1;j<n;j++)
+    {
+      if(v[index]>v[j])
+      index=j;
+    }swap(v[i],v[index]);
+  }
+  for(int i=0;i<n;i++)
+  cout<<v[i]<<" ";
+}
+time complexity:O(n^2)
+space Complexity:O(1)
